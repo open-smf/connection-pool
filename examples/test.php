4a9d452eae732eea3f45102f92506eb6f2233fa1
XieBiao <hhxsv5@sina.com>
2019-03-03 16:10:24 +0800

    update examples

--- examples/test.php
+++ examples/test.php
@@ -3,6 +3,7 @@
 include '../vendor/autoload.php';
 
 use Smf\ConnectionPool\CoroutineMySQLPool;
+use Swoole\Coroutine;
 use Swoole\Coroutine\MySQL;
 
 go(function () {
@@ -20,15 +21,24 @@ go(function () {
         'fetch_mode'  => true,
     ]);
 
-    for ($i = 0; $i < 10; $i++) {
-        go(function () use ($pool) {
-            /**@var MySQL $mysql */
-            $mysql = $pool->borrow();
-            defer(function () use ($pool, $mysql) {
-                $pool->return($mysql);
+    swoole_timer_tick(1000, function () use ($pool) {
+        var_dump('Current pool size: ' . $pool->getCurrentSize());
+    });
+
+    while (true) {
+        for ($i = 0; $i < mt_rand(10, 25); $i++) {
+            go(function () use ($pool) {
+                /**@var MySQL $mysql */
+                $mysql = $pool->borrow();
+                defer(function () use ($pool, $mysql) {
+                    $pool->return($mysql);
+                });
+                $ret = $mysql->query('select sleep(1),now() as now');
+                if (!isset($ret[0]['now'])) {
+                    var_dump("Invalid query result: \n" . print_r($ret, true));
+                }
             });
-            $ret = $mysql->query('select sleep(1)');
-            var_dump($ret);
-        });
+        }
+        Coroutine::sleep(1);
     }
 });
