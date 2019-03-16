468641961521c9f153550ffe7dc26e06b9ff9ec9
XieBiao <hhxsv5@sina.com>
2019-03-16 13:56:47 +0800

    add coroutine postgresql connector

---
+++ src/Connectors/CoroutinePostgreSQLConnector.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Smf\ConnectionPool\Connectors;
+
+use Swoole\Coroutine\PostgreSQL;
+
+class CoroutinePostgreSQLConnector implements ConnectorInterface
+{
+    public function connect(array $config)
+    {
+        if (!isset($config['connection_strings'])) {
+            throw new \InvalidArgumentException('The key "connection_string" is missing.');
+        }
+        $connection = new PostgreSQL();
+        $ret = $connection->connect($config['connection_strings']);
+        if ($ret === false) {
+            throw new \RuntimeException(sprintf('Failed to connect PostgreSQL server: %s', $connection->error));
+        }
+        return $connection;
+    }
+
+    public function disconnect($connection)
+    {
+        /**@var PostgreSQL $connection */
+    }
+
+    public function isConnected($connection): bool
+    {
+        /**@var PostgreSQL $connection */
+        return true;
+    }
+
+    public function reset($connection, array $config)
+    {
+        /**@var PostgreSQL $connection */
+    }
+}
